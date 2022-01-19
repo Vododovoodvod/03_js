251e0b51a19cd9098678bf96761e98ef48837a9c
Vododovoodvod <86209214+Vododovoodvod@users.noreply.github.com>
2022-01-19 20:30:14 +0100

    zadatak 2

---
+++ z2.js
@@ -0,0 +1,17 @@
+// Inicijalizacija varijabli?
+
+var firstName = "Tony";
+var secondName = "John";
+var lastName = "Doe";
+var price = 0;
+var discount = 78;
+var fullPrice = 0;
+var varBreak = [];
+var $myObject = {};
+
+
+console.log(price);
+console.log(lastName);
+
+
+// Kraj mog programa
