9cbb6a6705866dde5c16abcebf1cc70660c10537
Vododovoodvod <86209214+Vododovoodvod@users.noreply.github.com>
2022-02-02 21:10:43 +0100

    Odbrojevanje

--- my-web-app/app.js
+++ my-web-app/app.js
@@ -1,3 +1,4 @@
+/*
 //1
 console.log("WIndow width: " + window.innerWidth + ", Height: " + window.innerHeight);
 
@@ -13,4 +14,40 @@ window.setTimeout(testLog(), 2000);
 
 function testLog() {
     console.log("2 sec");
+}
+*/
+
+/*
+var appElement = document.getElementById("app");
+console.log(appElement);
+
+setTimeout(function() {
+    var newElement = document.createElement('div');
+    newElement.innerText = "Hello!";
+
+    appElement.appendChild(newElement);
+}, 2000)
+
+setTimeout(function() {
+    appElement.remove();
+}, 4000)
+*/
+
+var sec = 10;
+var appElement = document.getElementById("app");
+var newElement = document.createElement('div');
+newElement.innerText = sec;
+appElement.appendChild(newElement);
+
+var timer = setInterval(myTimer, 1000);
+
+function myTimer() {
+    sec--;
+    if (sec>=0){
+        newElement.innerText = sec;
+    }
+    else{
+        newElement.remove();
+        clearInterval(timer);
+    }
 }
